8408f8eb21020c5e32999edca6b40b6623ee9795
Joseph DeVita <jodvita@gmail.com>
2015-04-22 13:57:28 -0400

    lastest build

---
+++ mpros.c
@@ -0,0 +1,42 @@
+#include "csapp.h"
+void echo(int connfd);
+
+void sigchld_handler(int sig)
+{
+	while (waitpid (-1, 0, WNOHANG) > 0)
+		;
+	return;
+}
+
+int main(int argc, char **argv)
+{
+
+	int listenfd, connfd, port;
+	socklet_t clientlen = sizeof (struct sockaddr_in);
+	struct sockaddr_in clientaddr;
+
+	if(argc != 2) 
+	{
+		fprintf(stderr, "user: %s <port>\n", argv[0]);
+		exit(0);
+	}
+
+	port = atoi(argv[1]);
+
+	Signal(SIGCHILD, sigchld_handler);
+	listenfd = Open_listenfd(port);
+
+	while(1)
+	{
+		connfd = Accept(listenfd, (SA *) &clientaddr, &clientlen);
+
+		if(Fork() == 0) {
+			Close(listenfd);
+			echo(connfd);
+			Close(connfd);
+			exit(0);
+		}
+	}
+
+	Close(connfd);
+}
