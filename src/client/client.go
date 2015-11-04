08bacb4863c2d9a4180405261f5807df3e37b646
v1ta <jodvita@gmail.com>
2015-11-03 22:05:21 -0500

    flag parsing implemented

--- src/client/client.go
+++ src/client/client.go
@@ -5,45 +5,49 @@ import (
 			"fmt"
 			"strings"
 			"gopkg.in/mgo.v2/bson"
-			//"encoding/json"
-			//"github.com/LDCS/sflag"
+			"github.com/fatih/structs"
+			"strconv"
 )
 
-type Student struct{
-	NetID 	bson.ObjectId 	`json: "id" bson:"_id"`
-	Name 	string 			`json: "name"bson:"name"`
-	Major 	string 			`json: "major"bson:"major"`
-	Year 	int 			`json: "year"bson:"year"`
-	Grade 	int 			`json: "grade"bson:"grade"`
-	Rating 	string 			`json: "rating"bson:"rating"`
-}
+var dataFlag Student
+var m = structs.New(&dataFlag)
 
+type Student struct{
+	Id 		bson.ObjectId 	`json:"id" bson:"_id"`
+	NetID 	string 			`json:"netid" bson:"netid"`
+	Name 	string 			`json:"name" bson:"name"`
+	Major 	string 			`json:"major" bson:"major"`
+	Year 	int 			`json:"year" bson:"year"`
+	Grade 	int 			`json:"grade" bson:"grade"`
+	Rating 	string 			`json:"rating" bson:"rating"`
+}
 
 func (s *Student) String() string {
 	return fmt.Sprint("%+v\n",*s)
 }
 
-
-
 func (s *Student) Set(value string) error {
 
 	key := value[3:strings.Index(value,":")]
 	val := value[strings.Index(value,":")+1:]
+	field := m.Field(key)
 
-	fmt.Println(key)
-	fmt.Println(val[:len(val)-3])
-
+	if key == "Year" || key == "Grade"{
+		x, err := strconv.Atoi(val[:len(val)-3]) 
+		field.Set( x)
+		if err != nil {
+			panic(err)
+		}
+	}else{
+		field.Set(val[:len(val)-3])
+	}
+	
 	return nil
 }
-
-var dataFlag Student
-
 
 func init() {
 	flag.Var(&dataFlag, "data", "Student Object")
 }
-
-
 
 func main(){
     urlPtr := flag.String("url", "http://localhost:8080", "Usage: -url=<URL of the webservice>")
@@ -52,9 +56,25 @@ func main(){
 
     flag.Parse()
 
-    fmt.Println("url:", *urlPtr)
-    fmt.Println("method:", *methodPtr)
-    fmt.Println("year:", *yearPtr)
-    fmt.Println("data:", dataFlag)
-    fmt.Println("tail:", flag.Args())
+    //fmt.Println("url:", *urlPtr)
+    //fmt.Println("method:", *methodPtr)
+    //fmt.Println("year:", *yearPtr)
+    if *(&dataFlag.NetID) != ""{
+
+	    fields := m.Fields()
+	    field := m.Field("Id")
+	    field.Set(bson.NewObjectId())
+
+		for _, f := range fields {
+		    fmt.Printf("field name: %+v\n", f.Name())
+
+		    if f.IsExported() {
+		        fmt.Printf("value   : %+v\n", f.Value())
+		        fmt.Printf("is zero : %+v\n", f.IsZero())
+		   }
+		}
+    } 
+
+
+
 }
--- src/webservice/Mongo.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "mgo"
-
-type MongoConnection struct {
-	originalSession *mgo.Session
-}
